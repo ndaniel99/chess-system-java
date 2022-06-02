1ba5cc5aee4332b72dc7b29309ccd4eaa783dad1
Nuno Martins <ndanielmartins99@gmail.com>
2022-06-02 15:55:47 +0100

    Started creating piece and board classes

---
+++ src/boardgame/Piece.java
@@ -0,0 +1,29 @@
+package boardgame;
+
+public abstract class Piece {
+	protected Position position;
+	private Board board;
+	
+	public Piece(Board board) {
+		this.board = board;
+		position = null;
+	}
+
+	
+	protected Board getBoard() {
+		return board;
+	}
+
+
+
+//	public boolean[][] possibleMoves(){
+//	}
+//	
+//	public boolean possibleMove(Position position) {
+//		
+//	}
+//	
+//	public boolean isThereAnyPossibleMove() {
+//		
+//	}
+}
