d5b05496fb18f4730482ff570d6d19b1f5417e31
Nuno Martins <ndanielmartins99@gmail.com>
2022-06-02 16:57:16 +0100

    having some trouble

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -23,13 +23,16 @@ public class ChessMatch {
 		return mat;
 	}
 	
+	private void placeNewPiece(char column, int row, ChessPiece piece) {
+		board.placePiece(piece, new ChessPosition(column, row).toPosition());
+	}
+	
 	private void initialSetup() {
-		board.placePiece(new Rook(board,Color.WHITE),new Position(7,0));
-		board.placePiece(new Rook(board,Color.WHITE),new Position(7,7));
-		board.placePiece(new Rook(board,Color.BLACK),new Position(0,0));
-		board.placePiece(new Rook(board,Color.BLACK),new Position(0,7));
+		placeNewPiece('a',1,new Rook(board,Color.WHITE));
+//		placeNewPiece('h',1,new Rook(board,Color.WHITE));
+//		placeNewPiece('a',8,new Rook(board,Color.BLACK));
+//		placeNewPiece('h',8,new Rook(board,Color.BLACK));
 		
-		board.placePiece(new King(board,Color.BLACK),new Position(0,4));
-		board.placePiece(new King(board,Color.WHITE),new Position(7,4));
+		
 	}
 }
