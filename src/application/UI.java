6916cb955403abc5621d8e5267615c4ebd405720
Nuno Martins <ndanielmartins99@gmail.com>
2022-06-05 14:49:07 +0100

    Possible piece moves

--- src/application/UI.java
+++ src/application/UI.java
@@ -28,18 +28,22 @@ public class UI {
 	public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
 	public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
 
+	public static void clearScreen() {
+		System.out.print("\033[H\033[2J");
+		System.out.flush();
+	}
+
 	public static ChessPosition readChessPosition(Scanner sc) {
-	try {
-		String s = sc.nextLine();
-		char column = s.charAt(0);
-		int row = Integer.parseInt(s.substring(1));
-		return new ChessPosition (column,row);
+		try {
+			String s = sc.nextLine();
+			char column = s.charAt(0);
+			int row = Integer.parseInt(s.substring(1));
+			return new ChessPosition(column, row);
+		} catch (RuntimeException e) {
+			throw new InputMismatchException("Error reading chess position: Valid a1 to h8.");
+		}
 	}
-	catch (RuntimeException e) {
-		throw new InputMismatchException("Error reading chess position: Valid a1 to h8.");
-	}
-	}
-	
+
 	public static void printBoard(ChessPiece[][] pieces) {
 		for (int i = 0; i < pieces.length; i++) {
 			System.out.print((8 - i) + " ");
@@ -52,8 +56,6 @@ public class UI {
 		System.out.println("  a b c d e f g h");
 	}
 
-	
-	
 	private static void printPiece(ChessPiece piece) {
 		if (piece == null) {
 			System.out.print("-");
--- src/boardgame/Piece.java
+++ src/boardgame/Piece.java
@@ -16,14 +16,22 @@ public abstract class Piece {
 
 
 
-//	public boolean[][] possibleMoves(){
-//	}
-//	
-//	public boolean possibleMove(Position position) {
-//		
-//	}
-//	
-//	public boolean isThereAnyPossibleMove() {
-//		
-//	}
+	public abstract boolean[][] possibleMoves();
+	
+	public boolean possibleMove(Position position) {
+		return possibleMoves()[position.getRow()][position.getColumn()];
+	}
+	
+
+	public boolean isThereAnyPossibleMove() {
+		boolean mat[][] = possibleMoves(); 
+		for (int i = 0; i < mat.length ; i++) {
+			for (int j = 0; j < mat.length ; j++) {
+				if (mat[i][j]) {
+						return true;
+				}
+			}
+		}
+		return false;
+	}
 }
